55159823e3a086da7a43d78e065e0aa14caa6c8f
shreefinserv-office <shreefinserv.office@gmail.com>
2026-06-18 10:38:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // To push an update: bump APP_VERSION below, commit to GitHub.
 // Users clicking "Check for Update" will get the new version automatically.
 
-const APP_VERSION = '6.3.4';
+const APP_VERSION = '6.3.8';
 const CACHE_NAME  = 'gstaxofy-' + APP_VERSION;
 const CACHE_BASE  = 'gstaxofy';
 
